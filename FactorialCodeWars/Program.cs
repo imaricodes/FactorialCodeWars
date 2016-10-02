241d17701ab870cef3aa7705a7ed1f97b2d8e54f
imaricodes <imaricodes@gmail.com>
2016-10-01 18:29:52 -0700

    commit for push no changes

--- FactorialCodeWars/Program.cs
+++ FactorialCodeWars/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//CodeWars.com kata practice
+//Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#)
+ 
+
+
 namespace FactorialCodeWars
 {
     class Program
